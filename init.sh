39fd9d43153fe615051f7ed4bcf53e4870ac03bf
Michael Boone <fuag155555@gmail.com>
2013-03-30 19:20:21 -0700

    we can no glob our modules

--- init.sh
+++ init.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 # to install put this where your bash is sourced
-# [[ -f ~/flannel/init.sh ]] && . ~/flannel/init.sh fuzzy_plaid
+# [[ -f ~/.flannel/init.sh ]] && . ~/.flannel/init.sh fuzzy_plaid
 
 # mill some flannel, or wherever that wondermilk comes from
 flannel() {
-  local module="$1"; shift 
-
-  # did we already consume this?
-  if declare -f module_crumbs >/dev/null && module_crumbs "$module'$@'"; then
-    return 0
-  fi
-
-  # load our modules base files without nullglob returning literal string
   shopt -s nullglob
-  for file in ~/flannel/"$module"/*.{sh,bash}; do
-    . "$file"
+  local module_glob="$1"; shift 
+  for module in ~/.flannel/$module_glob; do
+    # did we already consume this?
+    if declare -f module_crumbs >/dev/null && module_crumbs "${module#~/.flannel}'$@'"; then
+      return 0
+    fi
+ 
+    # load our modules base files without nullglob returning literal string
+    for file in "$module"/*.{sh,bash}; do
+      . "$file"
+    done
+
+    shopt -u nullglob
+    # now load the modules requirements if it has any
+    [[ -f "$module"/init.flannel ]] && . "$module"/init.flannel "$@"
+    shopt -s nullglob
   done
   shopt -u nullglob
-
-  # now load the modules requirements if it has any
-  [[ -f ~/flannel/"$module"/init.flannel ]] && . ~/flannel/"$module"/init.flannel -c "$breadcrumbs$module'$@'" "$@"
 }
 
 # run our first config
