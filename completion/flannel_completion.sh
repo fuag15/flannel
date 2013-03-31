54fa8e990f536fa89c1457a03545c98872bc584e
Michael Boone <fuag155555@gmail.com>
2013-03-31 01:44:29 -0700

    very basic shell completion is now an option

--- completion/flannel_completion.sh
+++ completion/flannel_completion.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 ## initial code contributed by @odgrim
-__flannel_completion () {
+vest_completion () {
   shopt -s nullglob
-  case "${COMP_WORDS[COMP_CWORD-1]}" in
-    vest)
-      COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
-      COMPREPLY=("${COMPREPLY[@]#~/.flannel}")
-      COMPREPLY=("${COMPREPLY[@]%/}")
-    ;;
-  esac
+  COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
+  COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
+  COMPREPLY=("${COMPREPLY[@]%/}")
   shopt -u nullglob
 }
