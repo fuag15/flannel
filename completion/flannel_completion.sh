414dfd07bf248b0079464c17ed2cfc689b03226c
Michael Boone <fuag155555@gmail.com>
2013-03-31 01:49:23 -0700

    quick fix to not glom names together

--- completion/flannel_completion.sh
+++ completion/flannel_completion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## initial code contributed by @odgrim
-vest_completion () {
+completion_for_vest () {
   shopt -s nullglob
   COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
   COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
