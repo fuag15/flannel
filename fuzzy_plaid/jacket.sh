77e8b2eb36033d422726a430026629a7b812701c
Michael Boone <fuag155555@gmail.com>
2013-03-30 19:57:46 -0700

    noted bug but fixed prompt

--- fuzzy_plaid/jacket.sh
+++ fuzzy_plaid/jacket.sh
@@ -4,6 +4,12 @@ jacket() {
   # wear home config
   [[ -f ~/.flannelrc ]] && wear < ~/.flannelrc
 
+  # if we are setting a profile to stack
+  if [[ -n "$1" ]]; then
+    # wear our profile
+    [[ -f ~/."$1".flannelrc ]] && wear < ~/."$1".flannelrc
+  fi
+
   # wear patches
   [[ -f ./.plaid_patch ]] && wear < ./.plaid_patch
 }
--- prompt/colorize_prompt.sh
+++ prompt/colorize_prompt.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-
+# split up but not usable yet note \$(permission_aware_tail) will eval each time
+# but is broken because root has no ~/.flannel, see issue #5 for details
 colorize_prompt() {
     local user_and_host="\[\033[01;32m\]\u\[\033[01;31m\]@"
     local cur_location="\[\033[01;33m\]\w"
     local git_branch_color="\[\033[31m\]"
     local git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)\ /`'
-    
-    if [[ $UID -ne 0 ]]; then
-        local prompt_tail="\[\033[35m\]∫"
-    else 
-        local prompt_tail="\[\033[35m\]√"
-    fi
-
     local last_color="\[\033[00m\]"
-    export PS1="$user_and_host$cur_location $git_branch_color$git_branch$prompt_tail$last_color "
+    export PS1="$user_and_host$cur_location $git_branch_color$git_branch\$(permission_aware_tail)$last_color "
 }
