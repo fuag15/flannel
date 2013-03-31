2cc59b2a1c6865446b48b98d1a54d6ddaea148f4
Michael Boone <fuag155555@gmail.com>
2013-03-31 14:55:13 -0700

    some tidying to globstar bash 4 autocompletion

--- completion/flannel_completion.sh
+++ completion/flannel_completion.sh
@@ -4,12 +4,12 @@ completion_for_vest () {
   if [[ "${BASH_VERSION%%.*}" < 4 ]]; then # we are on bash less than 4, sad times
     shopt -s nullglob
     COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
-    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
     COMPREPLY=("${COMPREPLY[@]%/}")
+    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
     shopt -u nullglob
   else # we are on bash 4 yayy its globstar time
     shopt -s nullglob globstar
-    COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"**/)
+    COMPREPLY=(~/.flannel/**/"${COMP_WORDS[COMP_CWORD]}"*/)
     COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
     COMPREPLY=("${COMPREPLY[@]%/}")
     shopt -u nullglob globstar
