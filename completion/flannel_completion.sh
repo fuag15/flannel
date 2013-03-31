c5fcce76542b1406a00a0d0ed7eb9dc4c6ddae90
Michael Boone <fuag155555@gmail.com>
2013-03-31 02:44:41 -0700

    added some more aliases for my lazy self

--- completion/flannel_completion.sh
+++ completion/flannel_completion.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ## initial code contributed by @odgrim
 completion_for_vest () {
-  shopt -s nullglob
-  COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
-  COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
-  COMPREPLY=("${COMPREPLY[@]%/}")
-  shopt -u nullglob
+  if [[ "${BASH_VERSION%%.*}" < 4 ]]; then # we are on bash less than 4, sad times
+    shopt -s nullglob
+    COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
+    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
+    COMPREPLY=("${COMPREPLY[@]%/}")
+    shopt -u nullglob
+  else # we are on bash 4 yayy its globstar time
+    shopt -s nullglob globstar
+    COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"**/)
+    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
+    COMPREPLY=("${COMPREPLY[@]%/}")
+    shopt -u nullglob globstar
+  fi
 }
