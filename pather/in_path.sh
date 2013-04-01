8e0da802854b1e857c4850ce911852f069998835
Michael Boone <fuag155555@gmail.com>
2013-03-31 20:27:16 -0700

    doc stubs and made matcher match globs

--- pather/in_path.sh
+++ pather/in_path.sh
@@ -7,7 +7,7 @@
 # - in the middle or front of path?
 # - at the end of path?
 in_path() { # returns 0 if in path, 1 if not in path
-  if [[ "${!1}" == *"${2}:"* || "${!1}" == *"$2" ]]; then # our path contains it
+  if [[ "${!1}" == *${2}:* || "${!1}" == *$2 ]]; then # our path contains it
     return
   else # our path doesnt contain it
     return 1
