5b34896a2e557ca3c9e32e21cc2e164463005c02
Michael Boone <fuag155555@gmail.com>
2013-04-06 22:31:25 -0700

    added initial funcitons, need to test both requires and versions module then use in example and update docs

---
+++ fuzzy_plaid/clean_closet.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# sometimes your closet gets messy
+# this is a helper function that removes all versions of a module which are present
+# if it recieves a version it removes all versions but the specified version
+# syntax: `clean_closet <module_base> [version]`
+# for each thing in the module that isn't requested, if its in our closet, clear it
+clean_closet() {
+  # disable null globs returning literals, and enable glob extensions
+  shopt -s extglob
+  shopt -s nullglob
+
+  # each module that matches our params
+  for module in ~/.flannel/"$1"/!("$2"|*.flannel|*.md|*.sh|*.bash); do
+    # if this is in our path
+    if [[ "$PLAID_SPOOL" == *"${module#~/.flannel/}"* ]]; then
+      # remove it
+      flannel "${module#~/.flannel/}" clear
+    fi
+  done
+  # restore state
+  shopt -u extglob
+  shopt -u nullglob
+}
--- versions/catch_all.sh
+++ versions/catch_all.sh
@@ -2,15 +2,67 @@
 # this is a catch all comparator designed to handle all versions "smartly"
 # Syntax: `_flannel_catch_all_comparator <module_base> <comparator{<,>}> <target_version>
 # this attempts to smartly match the version by the following criteria
-# - remove all special characters and letters from the left for each version
-# - remove all special characters and letters from the right for each version
-# - compare result as an integer
-# - if equal
-#   - remove from the left what we were matching for both
-#   - if the left or right is empty
-#     - return left <comparator> right
-# - else
-#   - test with comparator and give 0 for true and 1 for false!
+# - while there are still characters left in either side
+#   - remove all special characters and letters from the left for each version
+#   - remove all special characters and letters from the right for each version
+#   - compare result as an integer
+#   - if equal
+#     - remove from the left what we were matching for both
+#     - if the left or right is empty
+#       - return left <comparator> right
+#   - else
+#     - test with comparator and give 0 for true and 1 for false!
 _flannel_catch_all_comparator() {
-  
+  # get our current and target full base
+  local current_base="$1"
+  local target_base="$3"
+
+  # set local vars for current manipulation of each base
+  local current_sub="$1"
+  local target_sub="$3"
+
+  # initialize a remainder var to take of the tail of a string
+  local remainder=""
+
+  # while there are still things to be compared
+  while [[ -n "$current_base" || -n "$target_base" ]]; do
+    # remove special characters and letters from left and right
+    current_base="${current_base##*[[:alpha:][:punct:]]}"
+    remainder="${current_base##*[[:digit:]]}"
+    current_sub="${sub%$remainder}"
+
+    target_base="${target_base##*[[:alpha:][:punct:]]}"
+    remainder="${target_base##*[[:digit:]]}"
+    target_sub="${sub%$remainder}"
+
+    # if current_base is null and we want less than, then tru
+    if [[ -n "$current_base" && "$2" == "<" ]]; then
+      return
+    else
+      return 1
+    fi
+
+    # if the current_target is null and we want greater than, then return true else false
+    if [[ -n "$current_target" && "$2" == ">" ]]; then
+      return
+    else
+      return 1
+    fi
+
+    # remove from base
+    current_base="${current_base#current_sub}"
+    target_base="${target_base#target_sub}"
+
+    # first we check if they are equal, if are we satisfied or not!
+    if (( current_sub == target_sub )); then
+      # they were equal, move on to the next part
+      continue
+    elif (( current_sub "$2" target_sub )); then
+      # satisfactory
+      return
+    else
+      # not satisfied!
+      return 1
+    fi
+  done
 }
