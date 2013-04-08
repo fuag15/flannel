62750d315a67d264db6d523afbf96d29c8f95712
Michael Boone <fuag155555@gmail.com>
2013-04-07 18:55:42 -0700

    finished spec for new pellets, need to impliment and update doc fixed loophole in module loading with sheep

--- pellets/lint_pellets.sh
+++ pellets/lint_pellets.sh
@@ -2,11 +2,14 @@
 # lint pellets should be colled before anything in a configuration except any calls to lint_pellet
 # lint pellets goes through our PLAID_PELLETS and removes and reverse dpendencies
 # if we are not called with clear thin it simple returns.
-# syntax: `lint_pellets <module> <version> [clear]
+# syntax: `lint_pellets <module> [clear]
 # first, check if we got a clear, if not return
 # next if its in our pellet path
-#   remove from plaid pellets
-#   clear the module!
+#   if we dont satisfy it wiht any existing module in PLAID_SHEEP
+#     remove from plaid pellets
+#     clear the module!
+#   else
+#     return
 lint_pellets() {
   # if we aren't clearing, we don't care
   if [[ "${@:(-1)}" != "clear" ]]; then
