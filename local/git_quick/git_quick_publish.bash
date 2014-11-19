02be559e2c800ffe8413fae6a76b380b6286e680
Michael Boone <fuag155555@gmail.com>
2014-11-18 23:02:47 -0800

    removed old repos

--- local/git_quick/git_quick_publish.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# quickly push develop to origin and master
-# usefull for projects im a lone wolf on
-git_quick_publish() {
-  git push origin develop
-  git push origin develop:master
-}
