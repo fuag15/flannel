123ac890bcee0dde6e707b2f0c9dc813c6e05c86
Brian Torres <odgrim.post@gmail.com>
2015-01-11 23:07:53 -0800

    add a concept of flannel home

--- init.bash
+++ init.bash
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # to install put this where your bash is sourced
 # [[ -f ~/.flannel/init.sh ]] && . ~/.flannel/init.bash
+export FLANNEL_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 
 shopt -s nullglob extglob
 # load our main module and flannel it
-for file in ~/.flannel/core/*.bash; do
+for file in $FLANNEL_HOME/core/*.bash; do
   . "$file"
 done
 shopt -u nullglob extglob
