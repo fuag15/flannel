40c76d3986a82927eb984690efe0028cbacb1a4c
Brian Torres <odgrim.post@gmail.com>
2015-01-12 21:12:45 -0800

    fix completions as well

--- core/completion/completion_for_vest.bash
+++ core/completion/completion_for_vest.bash
@@ -3,19 +3,19 @@
 # this handles bash completions for vest tasks
 # - gets all matching words in .flannel
 # - removes the trailing / 
-# - removed the preceeding ~/.flannel
+# - removed the preceeding $FLANNEL_HOME
 # If your cool and using bash >= 4 it will correct for submodules too
 _flannel_core_completion_for_vest () {
   if [[ "${BASH_VERSION%%.*}" < 4 ]]; then # we are on bash less than 4, sad times
     shopt -s nullglob
-    COMPREPLY=(~/.flannel/"${COMP_WORDS[COMP_CWORD]}"*/)
+    COMPREPLY=($FLANNEL_HOME/"${COMP_WORDS[COMP_CWORD]}"*/)
     COMPREPLY=("${COMPREPLY[@]%/}")
-    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
+    COMPREPLY=("${COMPREPLY[@]#$FLANNEL_HOME/}")
     shopt -u nullglob
   else # we are on bash 4 yayy its globstar time
     shopt -s nullglob globstar
-    COMPREPLY=(~/.flannel/**/"${COMP_WORDS[COMP_CWORD]}"*/)
-    COMPREPLY=("${COMPREPLY[@]#~/.flannel/}")
+    COMPREPLY=($FLANNEL_HOME/**/"${COMP_WORDS[COMP_CWORD]}"*/)
+    COMPREPLY=("${COMPREPLY[@]#$FLANNEL_HOME/}")
     COMPREPLY=("${COMPREPLY[@]%/}")
     shopt -u nullglob globstar
   fi
